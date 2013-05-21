5767655faff47bc6fd2cccc32279c9b35ac415fc
Christian Bullock <cbullock@neuone.com>
2013-05-21 15:17:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWorld::Application.config.secret_token = 'ec5d27823dadb5f743a4f06da1c86f6fb1107ce922fb9e34774e18de8b939b920e8f45232e96fa8240e680de29542394c5beb58d9d02f46b7b721cd2d1f7e2d3'
